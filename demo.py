264f573f08d3d5af3f8ef9e12a64c37696236edb
Rowida Alfrjani <r.alfrjani@sheffield.ac.uk>
2021-04-21 10:20:21 +0100

    first commit

---
+++ demo.py
@@ -0,0 +1,13 @@
+mylist=[]
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+for x in mylist:
+    print(x)
+mylist2=[3,4,5]
+for y in mylist2:
+    print(y)
